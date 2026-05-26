2feb245e2890fe8e21e2bc40e9302550952852bc
Trang A Gia <tranggia1106005@gmail.com>
2026-05-26 19:56:33 +0700

    add SinhVien Model :: getAll()

---
+++ app/models/SinhVienModel.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../app/core/DB.php';
+class sinhvienModel {
+    private $conn;
+    public function __construct()
+    {
+       $this->conn = connectDB::Connect();
+    }
+    public function getAllSinhVien()
+    {
+        $query = "SELECT * FROM tbl_sinhviens";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
