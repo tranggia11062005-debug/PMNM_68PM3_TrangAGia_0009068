a498bd6f3e77f8a2f962437d5ce47f4c897174be
Trang A Gia <tranggia1106005@gmail.com>
2026-06-05 10:30:04 +0700

    add masterlayout, partial

--- app/views/layout/masterlayout.php
+++ app/views/layout/masterlayout.php
@@ -3,18 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
-   style>
-        .content {
-            margin: 20px;
-        }
-    </style>
+    <title>Document</title>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .content{
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+</style>
 </head>
 <body>
-    <div><?php require_once '../app/views/layout/partial/header.php'; ?></div>
+     <?php require_once '../app/views/layout/partial/header.php'; ?>
     <div class="content">
-        <?php require_once '../app/views/' .$view. '.php'; ?>
+        <?php
+        require_once '../app/views/' . $view . '.php';
+        ?>
     </div>
-    <div><?php require_once '../app/views/layout/partial/footer.php'; ?></div>
+    <?php require_once '../app/views/layout/partial/footer.php'; ?>
 </body>
 </html>
